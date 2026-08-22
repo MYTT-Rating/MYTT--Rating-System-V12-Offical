980fbf9186bdd990dc0c6060d40e8bb147cc53d9
MYTT-Rating <hangtbabm@gmail.com>
2026-08-22 19:08:27 +0800

    Bust cache for corrected 2200 Immortal badge

--- marketplace.js
+++ marketplace.js
@@ -1,12 +1,12 @@
 (function () {
   "use strict";
 
-  const rankSrc = "rank-system-v27.js?v=20260822-10";
+  const rankSrc = "rank-system-v27.js?v=20260822-11";
   const rankCss = "rank-system-v27.css?v=20260822-5";
   const rankCssId = "mytt-rank-system-v27-style";
   const rankEffectsCss = "rank-effects-v31.css?v=20260822-7";
   const rankEffectsCssId = "mytt-rank-effects-v31-style";
-  const rankFixSrc = "rank-fix-v38.js?v=20260822-1";
+  const rankFixSrc = "rank-fix-v38.js?v=20260822-2";
   const rankFixCss = "rank-fix-v38.css?v=20260822-1";
   const rankFixCssId = "mytt-rank-fix-v38-style";
 
