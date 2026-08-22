383628bd563aa3851a37e095ee8e20a3a9cf8c0c
MYTT-Rating <hangtbabm@gmail.com>
2026-08-22 17:37:46 +0800

    Load animated rank glow effects

--- marketplace.js
+++ marketplace.js
@@ -1,9 +1,11 @@
 (function () {
   "use strict";
 
-  const rankSrc = "rank-system-v27.js?v=20260822-4";
-  const rankCss = "rank-system-v27.css?v=20260822-4";
+  const rankSrc = "rank-system-v27.js?v=20260822-5";
+  const rankCss = "rank-system-v27.css?v=20260822-5";
   const rankCssId = "mytt-rank-system-v27-style";
+  const rankEffectsCss = "rank-effects-v31.css?v=20260822-1";
+  const rankEffectsCssId = "mytt-rank-effects-v31-style";
 
   const coreSrc = "marketplace-core-v23.js?v=20260821-2";
   const polishSrc = "mobile-polish-v23.js?v=20260821-avatar-lazy-1";
@@ -24,6 +26,7 @@
   if (document.readyState === "loading") {
     ensureCss(polishCssId, polishCss);
     ensureCss(rankCssId, rankCss);
+    ensureCss(rankEffectsCssId, rankEffectsCss);
     document.write(`<script id="mytt-rank-system-v27" src="${rankSrc}"><\/script>`);
     document.write(`<script id="mytt-marketplace-core-v23" src="${coreSrc}"><\/script>`);
     document.write(`<script id="mytt-mobile-polish-v23" src="${polishSrc}"><\/script>`);
@@ -45,6 +48,7 @@
 
   ensureCss(polishCssId, polishCss);
   ensureCss(rankCssId, rankCss);
+  ensureCss(rankEffectsCssId, rankEffectsCss);
   loadScript("mytt-rank-system-v27", rankSrc)
     .then(() => loadScript("mytt-marketplace-core-v23", coreSrc))
     .then(() => loadScript("mytt-mobile-polish-v23", polishSrc))
