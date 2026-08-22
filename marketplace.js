928b06c297bb79fce88174e17bd84b7fc8884af5
MYTT-Rating <hangtbabm@gmail.com>
2026-08-22 16:56:33 +0800

    Refresh MYTT rank system assets

--- marketplace.js
+++ marketplace.js
@@ -1,8 +1,8 @@
 (function () {
   "use strict";
 
-  const rankSrc = "rank-system-v27.js?v=20260822-1";
-  const rankCss = "rank-system-v27.css?v=20260822-1";
+  const rankSrc = "rank-system-v27.js?v=20260822-2";
+  const rankCss = "rank-system-v27.css?v=20260822-2";
   const rankCssId = "mytt-rank-system-v27-style";
 
   const coreSrc = "marketplace-core-v23.js?v=20260821-2";
